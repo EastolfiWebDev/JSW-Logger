e4f9c9f7dfe659aea68843780df2c7c6d42b733d
eastolfi <astolfi_wotc@hotmail.com>
2016-10-23 15:13:46 +0000

    feat: Add browser support

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-var path = require("path"),
-    fs = require('fs-extra'),
-    _ = require("lodash"),
-    winston = require("winston"),
-    winstonLogger = winston.Logger;
-
-
-var Logger = require('./lib/JSW-Logger')(winstonLogger, winston, path, fs, _);
+var Logger = require('./src/JSW-Logger');
 
 module.exports = Logger;
+
+if (window) window.Logger = Logger;
